92aff9cb7b5b2a75a65599a94643b54bba6e3ae4
Noah <noah.moussaoui@student.uclouvain.be>
2024-11-07 20:40:04 +0100

    Quizz 25%

---
+++ src/main/java/Quizz/Flipper.java
@@ -0,0 +1,35 @@
+package Quizz;
+
+
+// SEE THE explanations in the Videos
+
+public class Flipper {
+
+    public static int run(char[][] map) {
+        int n = map.length;
+        int m = map[0].length;
+        int x = 0, y = 0;
+
+        boolean[][] visited = new boolean[n][m];
+        int count = 0;
+
+        while (x >= 0 && x < n && y >= 0 && y < m) {
+            if (!visited[x][y]) {
+                visited[x][y] = true;
+                count++;
+            }
+
+            if (map[x][y] == '/') {
+                x--;
+                y--;
+            } else if (map[x][y] == '\\') {
+                x++;
+                y++;
+            } else {
+                y++;
+            }
+        }
+
+        return count;
+    }
+}
